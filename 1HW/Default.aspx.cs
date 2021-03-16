8fdb9767c4ba72ef58b3cb86cf3f8d6cae9a84c3
錦皇 吳 <107111238@mail.oit.edu.tw>
2021-03-16 15:38:29 +0800

    第二周 第一次作業

--- 1HW/Default.aspx.cs
+++ 1HW/Default.aspx.cs
@@ -5,10 +5,23 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace _1HW {
-    public partial class Default : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
-
+namespace _1HW
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int i = 31;
+            string answer = "";
+            if (i % 1 == i || i % i == 1)
+            {
+                answer = "NO";
+            }
+            else
+            {
+                answer = "YES";
+            }
+            Response.Write(answer);
         }
     }
 }
